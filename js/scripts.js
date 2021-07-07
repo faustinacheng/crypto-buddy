10fefaed516fbe842ef5bb231d13b411d5e617e8
Pratyush K. Patnaik <78687109+Pratyush-exe@users.noreply.github.com>
2021-07-07 20:29:29 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -14,30 +14,76 @@ resultsDisplay.style.visibility = "hidden";
 
 // Code for Top 5
 
-var list = [document.getElementById("l1"), document.getElementById("l2"), document.getElementById("l3"), document.getElementById("l4"), document.getElementById("l5")];
+var l1 = document.getElementById("l1");
+var l2 = document.getElementById("l2");
+var l3 = document.getElementById("l3");
+var l4 = document.getElementById("l4");
+var l5 = document.getElementById("l5");
 
-async function calc_change(name){
-  const data = await fetch("https://api.coincap.io/v2/assets/"+name);
-  const DATA = await data.json();
+async function get_trending() {
+  let search = "https://api.coingecko.com/api/v3/search/trending";
+  const trending = await fetch(search);
+  const TRENDING = await trending.json();
 
-  var SYM = DATA.data.symbol;
-  var USD = DATA.data.priceUsd;
-  var PER = DATA.data.changePercent24Hr;
-  var res = [SYM.toString(), USD, PER];
-  return res;
-};
+  let trendingCoins = [];
 
-async function top5(){
-  var res = [calc_change("bitcoin"), calc_change("ethereum"), calc_change("dogecoin"), calc_change("tether"), calc_change("binance-coin")];
-  for(let i=0; i<5 ; i++){
-    res[i].then(
-      function(value){
-        list[i].textContent = value[0] + " : " + parseFloat(value[1]).toFixed(2).toString() + "$ : " + parseFloat(value[2]).toFixed(2).toString() + "%";
-      });}};
+  for (let i = 0; i < TRENDING.coins.length; i++) {
+    trendingCoins.push(TRENDING.coins[i].item.id);
+  }
 
-setInterval(top5, 1000);
+  let itemSearch = "";
+  let listdata = "";
+  let ListData = "";
+  let name, symbol;
 
-// Code for Search Crypto
+  for (let i = 0; i < trendingCoins.length; i++) {
+    itemSearch =
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=" +
+      trendingCoins[i] +
+      "&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h%2C7d";
+    listdata = await fetch(itemSearch);
+    ListData = await listdata.json();
+
+    name = ListData[0].name;
+    symbol = ListData[0].symbol;
+    let image = ListData[0].image;
+    let price = ListData[0].current_price;
+    let change24hr =
+      ListData[0].price_change_percentage_24h_in_currency.toFixed(2);
+    let change7d =
+      ListData[0].price_change_percentage_7d_in_currency.toFixed(2);
+
+    let trendingList = document.getElementById("trending-coins");
+    var li = document.createElement("li");
+    li.setAttribute("class", "list-item");
+
+    trendingList.appendChild(li);
+    li.innerHTML =
+      li.innerHTML +
+      `<div class="list-row list-rank">${
+        i + 1
+      }</div><img class="list-row list-icon" src=${image}/><div class="list-row list-symbol">${symbol}</div><div class="list-row list-name">${name}</div><div class="list-row list-price">${price}</div><div class="list-row change-24hr">${
+        change24hr + "%"
+      }</div><div class="list-row change-7d">${change7d + "%"}</div>`;
+      var someList = document.getElementById("trending-coins").getElementsByTagName("li");
+      if(parseFloat(someList[i].getElementsByClassName("list-row change-24hr")[0].innerText) < 0){
+        someList[i].getElementsByClassName("list-row change-24hr")[0].style.color = "#f23a3a";
+        someList[i].getElementsByClassName("list-row change-7d")[0].style.color = "#f23a3a";
+      }
+      else{
+        var temp = someList[i].getElementsByClassName("list-row change-24hr")[0].innerText;
+        someList[i].getElementsByClassName("list-row change-24hr")[0].innerText = "+" + temp;
+        someList[i].getElementsByClassName("list-row change-24hr")[0].style.color = "#1bb544";
+
+        var temp = someList[i].getElementsByClassName("list-row change-7d")[0].innerText;
+        someList[i].getElementsByClassName("list-row change-7d")[0].innerText = "+" + temp;
+        someList[i].getElementsByClassName("list-row change-7d")[0].style.color = "#1bb544";
+      }
+    
+  }
+}
+
+get_trending();
 
 document.querySelector("#search_data").addEventListener("keyup", (event) => {
   if (event.key !== "Enter") return;
@@ -84,12 +130,14 @@ search_button.onclick = function () {
       var change_data =
         ((DATA[0].price_usd - LastData.rate) / LastData.rate) * 100;
       if (change_data < 0) {
-        change.style.color = "#FF0000";
+        change.style.color = "#f23a3a";
+        sign = "-";
       } else {
-        change.style.color = "#00FF00";
+        change.style.color = "#1bb544";
+        sign = "+";
       }
       change.textContent =
-        change_data.toFixed(2).toString() + "%";
+        sign + Math.abs(change_data).toFixed(2).toString() + "%";
 
       for (let i = 0; i < IconData.length; i++) {
         if (IconData[i].asset_id == search_data.value) {
