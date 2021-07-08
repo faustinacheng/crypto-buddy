2711f6d2c9c64a88c89220412412683f18717c90
Faustina Cheng <chengfaustina@gmail.com>
2021-07-08 09:36:22 +0800

    Change to CoinGecko API and fix colours on percents

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,29 @@
-var api = "86BD34C5-A59B-4D68-B6C1-CECC1275E7A3";
-// var api = "52FE8C2C-9ACD-44BC-BE63-F362B9AF4B17";
-var search_button = document.getElementById("button");
-var search_data = document.getElementById("search_data");
-var searchSYM = document.getElementById("id");
-var searchNAME = document.getElementById("name");
-var currentPrice = document.getElementById("current-price");
-var lastPrice = document.getElementById("last-day-price");
-var change = document.getElementById("change");
-var cryptoImage = document.getElementById("cryptoImage");
-var resultsDisplay = document.getElementById("resultsDisplay");
+let search_button = document.getElementById("button");
+let search_data = document.getElementById("search_data");
+let searchSYM = document.getElementById("id");
+let searchNAME = document.getElementById("name");
+let currentPrice = document.getElementById("current-price");
+let marketCap = document.getElementById("market-cap");
+let marketCap24hr = document.getElementById("market-cap-24hr");
+let high24hr = document.getElementById("high-24hr");
+let low24hr = document.getElementById("low-24hr");
+let price24hr = document.getElementById("price-24hr");
+let price7d = document.getElementById("price-7d");
+let price1h = document.getElementById("price-1h");
+let cryptoImage = document.getElementById("cryptoImage");
+let resultsDisplay = document.getElementById("resultsDisplay");
+
+let numberFormat = new Intl.NumberFormat("en-US");
 
 resultsDisplay.style.visibility = "hidden";
 
-// Code for Top 5
+// Code for Trending
 
-var l1 = document.getElementById("l1");
-var l2 = document.getElementById("l2");
-var l3 = document.getElementById("l3");
-var l4 = document.getElementById("l4");
-var l5 = document.getElementById("l5");
+let listItems = document.getElementsByClassName("list-item");
 
 async function get_trending() {
   let search = "https://api.coingecko.com/api/v3/search/trending";
-  const trending = await fetch(search);
+  const trending = await fetch(search, { dataType: "jsonp" });
   const TRENDING = await trending.json();
 
   let trendingCoins = [];
@@ -36,68 +37,38 @@ async function get_trending() {
   let ListData = "";
   let name, symbol;
 
-  for (let i = 0; i < trendingCoins.length; i++) {
-    itemSearch =
-      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=" +
-      trendingCoins[i] +
-      "&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h%2C7d";
-    listdata = await fetch(itemSearch);
-    ListData = await listdata.json();
+  async function refreshList() {
+    for (let i = 0; i < trendingCoins.length; i++) {
+      itemSearch =
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=" +
+        trendingCoins[i] +
+        "&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h%2C7d";
+      listdata = await fetch(itemSearch);
+      ListData = await listdata.json();
 
-    name = ListData[0].name;
-    symbol = ListData[0].symbol;
-    let image = ListData[0].image;
-    let price = ListData[0].current_price;
-    let change24hr =
-      ListData[0].price_change_percentage_24h_in_currency.toFixed(2);
-    let change7d =
-      ListData[0].price_change_percentage_7d_in_currency.toFixed(2);
+      name = ListData[0].name;
+      symbol = ListData[0].symbol;
+      let image = ListData[0].image;
+      let price = ListData[0].current_price;
+      let change24hr = ListData[0].price_change_percentage_24h_in_currency;
+      let change7d = ListData[0].price_change_percentage_7d_in_currency;
 
-    let trendingList = document.getElementById("trending-coins");
-    var li = document.createElement("li");
-    li.setAttribute("class", "list-item");
-
-    trendingList.appendChild(li);
-    li.innerHTML =
-      li.innerHTML +
-      `<div class="list-row list-rank">${
+      listItems[i].innerHTML = `<div class="list-row list-rank">${
         i + 1
-      }</div><img class="list-row list-icon" src=${image}/><div class="list-row list-symbol">${symbol}</div><div class="list-row list-name">${name}</div><div class="list-row list-price">${price}</div><div class="list-row change-24hr">${
-        change24hr + "%"
-      }</div><div class="list-row change-7d">${change7d + "%"}</div>`;
-    var someList = document
-      .getElementById("trending-coins")
-      .getElementsByTagName("li");
-    if (
-      parseFloat(
-        someList[i].getElementsByClassName("list-row change-24hr")[0].innerText
-      ) < 0
-    ) {
-      someList[i].getElementsByClassName(
-        "list-row change-24hr"
-      )[0].style.color = "#f23a3a";
-      someList[i].getElementsByClassName("list-row change-7d")[0].style.color =
-        "#f23a3a";
-    } else {
-      var temp = someList[i].getElementsByClassName("list-row change-24hr")[0]
-        .innerText;
-      someList[i].getElementsByClassName("list-row change-24hr")[0].innerText =
-        "+" + temp;
-      someList[i].getElementsByClassName(
-        "list-row change-24hr"
-      )[0].style.color = "#1bb544";
+      }</div><img class="list-row list-icon" src=${image}/><div class="list-row list-symbol">${symbol}</div><div class="list-row list-name">${name}</div><div class="list-row list-price">${price}</div><div class="list-row change-24hr">${change24hr}</div><div class="list-row change-7d">${change7d}</div>`;
 
-      var temp =
-        someList[i].getElementsByClassName("list-row change-7d")[0].innerText;
-      someList[i].getElementsByClassName("list-row change-7d")[0].innerText =
-        "+" + temp;
-      someList[i].getElementsByClassName("list-row change-7d")[0].style.color =
-        "#1bb544";
+      stylePosNeg(listItems[i].querySelector(".change-24hr"));
+
+      stylePosNeg(listItems[i].querySelector(".change-7d"));
     }
   }
+  // setInterval(refreshList, 1000);
+  refreshList();
 }
-
+// setInterval(get_trending, 1000);
 get_trending();
+
+// Code for search results
 
 document.querySelector("#search_data").addEventListener("keyup", (event) => {
   if (event.key !== "Enter") return;
@@ -105,65 +76,79 @@ document.querySelector("#search_data").addEventListener("keyup", (event) => {
   event.preventDefault();
 });
 
-search_button.onclick = function () {
-  var tosearch =
-    "https://rest.coinapi.io/v1/assets/" + search_data.value + "?apikey=" + api;
-  var iconsearch =
-    "https://rest.coinapi.io/v1/assets/icons/" +
-    search_data.value +
-    "?apikey=" +
-    api;
+let coinsListSearch =
+  "https://api.coingecko.com/api/v3/coins/list?include_platform=false";
 
+let symbolToID = [];
+
+const getCoinsList = async () => {
+  const response = await fetch(coinsListSearch);
+  response.json().then((coins_json) => {
+    symbolToID = coins_json;
+  });
+};
+
+getCoinsList();
+
+search_button.onclick = function () {
   async function getData() {
+    let search_result = symbolToID.find((coin) => {
+      return (
+        coin.symbol.toLowerCase() === search_data.value.toLowerCase() ||
+        coin.name.toLowerCase() === search_data.value.toLowerCase()
+      );
+    });
+
+    let tosearch =
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=" +
+      search_result.id +
+      "&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d";
+
     const data = await fetch(tosearch);
     const DATA = await data.json();
-
-    var yesterday = new Date();
-    yesterday.setDate(yesterday.getDate() - 1);
-    var lastPriceSearch =
-      "https://rest.coinapi.io/v1/exchangerate/" +
-      search_data.value +
-      "/USD/?time=" +
-      yesterday.toISOString() +
-      "&apikey=" +
-      api;
-    const lastdata = await fetch(lastPriceSearch);
-    const LastData = await lastdata.json();
-
-    const icondata = await fetch(iconsearch);
-    const IconData = await icondata.json();
 
     if (DATA.length == 0) {
       alert("Error: Symbol does not exist!");
     } else {
-      searchSYM.textContent = DATA[0].asset_id;
+      cryptoImage.src = DATA[0].image
+        ? DATA[0].image
+        : "https://upload.wikimedia.org/wikipedia/commons/b/b4/Circle_question_mark.png";
+      searchSYM.textContent = DATA[0].symbol.toUpperCase();
       searchNAME.textContent = DATA[0].name;
-      currentPrice.textContent =
-        DATA[0].price_usd.toFixed(2).toString() + " USD";
-      lastPrice.textContent = LastData.rate.toFixed(2).toString() + " USD";
-      var change_data =
-        ((DATA[0].price_usd - LastData.rate) / LastData.rate) * 100;
-      if (change_data < 0) {
-        change.style.color = "#f23a3a";
-        sign = "-";
-      } else {
-        change.style.color = "#1bb544";
-        sign = "+";
-      }
-      change.textContent =
-        sign + Math.abs(change_data).toFixed(2).toString() + "%";
+      currentPrice.textContent = DATA[0].current_price.toLocaleString("en-US", {
+        maximumSignificantDigits: 21,
+      });
+      marketCap.textContent = DATA[0].market_cap.toLocaleString("en-US", {
+        maximumSignificantDigits: 21,
+      });
+      marketCap24hr.textContent = DATA[0].market_cap_change_percentage_24h;
+      stylePosNeg(marketCap24hr);
+      high24hr.textContent = `${DATA[0].high_24h.toLocaleString("en-US", {
+        maximumSignificantDigits: 8,
+      })} / ${DATA[0].low_24h.toLocaleString("en-US", {
+        maximumSignificantDigits: 8,
+      })}`;
+      price24hr.textContent = DATA[0].price_change_percentage_24h_in_currency;
+      stylePosNeg(price24hr);
+      price7d.textContent = DATA[0].price_change_percentage_7d_in_currency;
+      stylePosNeg(price7d);
+      price1h.textContent = DATA[0].price_change_percentage_1h_in_currency;
+      stylePosNeg(price1h);
 
-      for (let i = 0; i < IconData.length; i++) {
-        if (IconData[i].asset_id == search_data.value) {
-          cryptoImage.src = IconData[i].url;
-          break;
-        } else {
-          cryptoImage.src =
-            "https://upload.wikimedia.org/wikipedia/commons/b/b4/Circle_question_mark.png";
-        }
-      }
       resultsDisplay.style.visibility = "visible";
     }
   }
   getData();
 };
+
+function stylePosNeg(element) {
+  if (element.textContent < 0) {
+    element.style.color = "#f23a3a";
+    caret = `<span class="material-icons" style="color: #f23a3a; font-size: 100%;">arrow_drop_down</span>`;
+  } else {
+    element.style.color = "#1bb544";
+    caret = `<span class="material-icons" style="color: #1bb544; font-size: 100%;">arrow_drop_up</span>`;
+  }
+  element.innerHTML =
+    caret + Math.abs(element.textContent).toFixed(2).toString() + "%";
+}
